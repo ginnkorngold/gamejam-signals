d7b842b58b4c5ce89864cfbcc8a1e6de372f7df8
ljusica <jessiicaceder@hotmail.com>
2022-02-22 16:09:25 +0100

    Player movement + gravity manipulation

---
+++ Assets/Jessica/PlayerMovement.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public bool facingRight;
+    
+    public float speed;
+    public float jumpForce;
+    public float checkRadius;
+
+    public Transform groundedCheck;
+    public LayerMask ground;
+
+    private bool isGrounded;
+    
+    private float moveInput;
+
+    private int numberOfJumps = 1;
+    
+    private Rigidbody2D rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundedCheck.position, checkRadius, ground);
+
+        Move();
+    }
+
+    void Update()
+    {
+        Jump();
+    }
+
+    private void Move()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+
+        rigidBody.velocity = new Vector2(moveInput * speed, rigidBody.velocity.y);
+
+        if (!facingRight && moveInput > 0)
+        {
+            FlipSprite();
+        }
+        else if (facingRight && moveInput < 0)
+        {
+            FlipSprite();
+        }
+    }
+
+    void FlipSprite()
+    {
+        facingRight = !facingRight;
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && numberOfJumps > 0)
+        {
+            rigidBody.velocity = Vector2.up * jumpForce;
+            numberOfJumps--;
+        }
+
+        if (isGrounded)
+        {
+            numberOfJumps = 1;
+        }
+    }
+}
