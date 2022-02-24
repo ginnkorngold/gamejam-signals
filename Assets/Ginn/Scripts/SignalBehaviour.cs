89d4d731db1d820cbee3e54cb17342b22e7798c2
ginnkorngold <38727595+ginnkorngold@users.noreply.github.com>
2022-02-24 17:15:20 +0100

    Level one

--- Assets/Ginn/Scripts/SignalBehaviour.cs
+++ Assets/Ginn/Scripts/SignalBehaviour.cs
@@ -12,7 +12,10 @@ public class SignalBehaviour : MonoBehaviour
     public GameObject doorOneOpen;
     public GameObject doorOneClosed;
 
-    //[Header("Level one")]
+    [Header("Level one")]
+    public float speed;
+    public GameObject platform;
+    public GameObject conveyer;
 
 
 
@@ -31,6 +34,10 @@ public class SignalBehaviour : MonoBehaviour
             {
                 signalActive = true;
             }
+            if(insideTrigger == false)
+            {
+                signalActive = false;
+            }
         }
     }
 
@@ -39,6 +46,7 @@ public class SignalBehaviour : MonoBehaviour
         if (collision.gameObject.CompareTag("Lever"))
         {
             insideTrigger = false;
+            signalActive = false;
         }
     }
 
@@ -47,10 +55,11 @@ public class SignalBehaviour : MonoBehaviour
         if (collision.gameObject.CompareTag("Lever"))
         {
             insideTrigger = true;
+            Debug.Log("Inside trigger for lever");
         }
         if(collision.gameObject.CompareTag("Door"))
         {
-            SceneManager.LoadScene(+1);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 
@@ -63,6 +72,30 @@ public class SignalBehaviour : MonoBehaviour
                 doorOneClosed.SetActive(false);
                 doorOneOpen.SetActive(true);
             }
+            if(collision.gameObject.name == "Lever02")
+            {
+                platform.transform.position = new Vector2(platform.transform.position.x, -3f);
+            }
+            if (collision.gameObject.name == "Lever03")
+            {
+                conveyer.transform.position = new Vector2(-2.5f, conveyer.transform.position.y);
+            }
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "MoveablePlatform")
+        {
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x, 2f);
+            platform.transform.position = new Vector2(platform.transform.position.x, 2f);
+        }
+
+        if(collision.gameObject.name == "Basket")
+        {
+            gameObject.transform.position = new Vector2(5.15f, gameObject.transform.position.y);
+            conveyer.transform.position = new Vector2(5.15f, gameObject.transform.position.y);
         }
     }
 }
