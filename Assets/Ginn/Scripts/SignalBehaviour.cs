74397623fcb8e838a13ddfde85a96d4faae7c59f
ginnkorngold <38727595+ginnkorngold@users.noreply.github.com>
2022-02-24 14:58:06 +0100

    audio/particles

--- Assets/Ginn/Scripts/SignalBehaviour.cs
+++ Assets/Ginn/Scripts/SignalBehaviour.cs
@@ -3,21 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 public class SignalBehaviour : MonoBehaviour
 {
-    public ParticleSystem signalParticals;
-    private bool insideTrigger;
+    public GameObject signal;
+    public ParticleSystem signalParticles;
+    public bool insideTrigger;
+    public bool signalActive;
+    private Vector3 scale;
+    private float speed = 2f;
 
     // Start is called before the first frame update
     void Start()
     {
+        scale = signal.transform.localScale;
         insideTrigger = false;
+        signalActive = false;
+        signal.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Mouse0))
         {
-            signalParticals.Play();
+            signalParticles.Play();
+            //StartCoroutine(signalling());
             if(insideTrigger == true)
             {
                 Debug.Log("behaviour happening");
@@ -39,5 +47,20 @@ public class SignalBehaviour : MonoBehaviour
         {
             insideTrigger = true;
         }
+    }
+
+    private IEnumerator signalling()
+    {
+        signalActive = true;
+        signal.SetActive(true);
+        scale.x += Time.deltaTime * speed;
+        scale.y += Time.deltaTime * speed;
+        signal.transform.localScale = scale;
+
+        yield return new WaitForSeconds(5f);
+        signal.SetActive(false);
+        scale.x = 1f;
+        scale.y = 1f;
+        signalActive = false;
     }
 }
